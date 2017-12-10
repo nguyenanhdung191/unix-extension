993532e5974fe95b85ecab5ea29020d734df3741
Dung <nguyenanhdung191@gmail.com>
2017-12-10 10:11:52 +0700

    edit script.js

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
+console.log("Extension started!!");
+
 const buy = () => {
     $.post('ico', {
         unx_amount: "100",
-        captcha_secret: "ld8bqvrh",
-        captcha_key2: "1064"
+        captcha_secret: $('[name="captcha_secret"]').val(),
+        captcha_key2: $('[name="captcha_key2"]').val(),
     }, res => {
-        console.log(res);
+        if (res.success) {
+            $('[name="captcha_secret"]').val(res.captcha_secret);
+            $('#captcha-img').attr('src', '/img/captchas/' + res.captcha_secret + '.png');
+            console.log("SUCCESS: ", res);
+        }
+        else {
+            console.log("ERROR: ", res);
+        }
+
     })
 };
 
 $.get("../ico/info", json => {
-    let icoTime = parseInt(json.next_ico_date.from_timestamp);
-
+    let icoTime = parseInt(json.next_ico_date.from_timestamp) - 5000;
     let ticker = setInterval(() => {
         let currentTime = Date.now();
         if (currentTime >= icoTime) {
-            clearInterval(ticker);
+            let i = 0;
             buy();
-            setTimeout(buy, 5500);
-            setTimeout(buy, 10500);
-            setTimeout(buy, 15500);
-            setTimeout(buy, 20500);
-            setTimeout(buy, 25500);
-            setTimeout(buy, 30500);
-            setTimeout(buy, 35500);
-            setTimeout(buy, 40500);
-            setTimeout(buy, 45500);
-            setTimeout(buy, 50500);
-            setTimeout(buy, 55500);
-            setTimeout(buy, 60500);
-            setTimeout(buy, 65500);            
+            for (let j = 1; j <= 70; j++) {
+                i += 1000;
+                setTimeout(buy, i);
+            }
+            clearInterval(ticker);
         }
     }, 50);
 });
